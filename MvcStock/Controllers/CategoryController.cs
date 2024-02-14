fb84fd44c97200d5031ad9d3a6e614859eaea913
Begüm Yünkül <yunkulbegum@gmail.com>
2024-02-14 19:16:28 +0300

    tablolara veriler getirildi.

---
+++ MvcStock/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStock.Models.Entity;
+
+namespace MvcStock.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.CATEGORIES.ToList();
+            return View(degerler);
+        }
+    }
+}
