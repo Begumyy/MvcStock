229e1067ada95c7b607e28b90bfb24de7a617438
Begüm Yünkül <yunkulbegum@gmail.com>
2024-02-20 01:51:18 +0300

    popup eklendi.

---
+++ MvcStock/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using MvcStock.Models.Entity;
+
+namespace MvcStock.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SATISLAR p)
+        {
+            db.SATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
